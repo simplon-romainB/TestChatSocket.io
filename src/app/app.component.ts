4e40b9a3e28fd04a94f391c8261cda06c57ac45f
simplon-romainB <neriaa@hotmail.fr>
2017-01-26 15:05:04 +0100

    Update app.component.ts
    commentaires

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,13 @@ export class AppComponent {
 tabUsers = [];
 b= [];
 mess:string;
-
+/* il s'agit de l'abonné chargé de surveiller les messages envoyés par le serveur à travers un observable*/
 a = this.chat2service.getMessages().subscribe(data => {
     this.b.push(data);
     console.log(this.b);
 
   })
+/* cet abonné suit les connexions d'utilisateurs */
 usersOnline = this.chat2service.whoThere().subscribe(data =>{
   this.tabUsers.push(data);
   })
@@ -33,14 +34,16 @@ constructor(private chat2service:Chat2Service){}
 ngOnInit() {
 
 }
+/* fonction enclanchant l'emitter qui se situe dans le service afin d'envoyer les messages */
   sendMessages() {
     this.data.message = this.mess;
     this.chat2service.sendMessage(this.data);
     this.mess = '';
 
   }
-
+/* fonction inutile
   whoThere() {
     this.chat2service.whoThere()
   }
 }
+ */
